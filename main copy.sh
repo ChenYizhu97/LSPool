dfbae9486a518e3c5944924edf2862254c716287
ChenYizhu <cyzhaa@gmail.com>
2024-06-25 19:55:54 +0800

    remove duplicates

--- main copy.sh
+++
@@ -1,7 +0,0 @@
-log=logs/18_04_24/
-file=diff_norm
-#dataset=ENZYMES"FRANKENSTEIN""PROTEINS" 
-for dataset in  "PROTEINS" 
-do
-python3 main.py  --logging $log$file --pooling lspool --dataset $dataset "
-done
